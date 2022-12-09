6e171dd4ee9edb150091b529af19777180f2575b
Ishraaq Parvez <i4ishraaq@gmail.com>
2022-12-10 03:42:35 +0530

    commit

--- Java.java
+++ Java.java
@@ -2,8 +2,21 @@ import java.util.*;
 
 class Main{
     public class VectorDemo{
-        ArrayList<Integer> arr = new ArrayList<Integer>(); 
-        Arr
+        Vector<Integer> arr = new Vector<Integer>(); 
+        
+    }
+    public class PriorityQueueDemo{
+        PriorityQueue<E> pq = new PriorityQueue<E>();
+        PriorityQueue<E> pq = new PriorityQueue<E>(Collection<E> c);
+        PriorityQueue<E> pq = new PriorityQueue<E>(int initialCapacity);
+        PriorityQueue<E> pq = new PriorityQueue(int initialCapacity, Comparator<E> comparator);
+        PriorityQueue<E> pq = new PriorityQueue(PriorityQueue<E> c);
+        pq.add(1); // adds element
+        pq.peek(); // returns top
+        pq.poll(); //returns top and pops
+        pq.clear(); 
+        pq.contains(k); 
+        pq.isEmpty(); 
     }
     public static void main(String[] args){
         return 0;
